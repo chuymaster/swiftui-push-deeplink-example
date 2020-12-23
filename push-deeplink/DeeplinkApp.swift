cbc8f94702c97a6e0dc0a74a6a657e6bf76808df
chuymaster <chatchai.lokniyom@quipper.com>
2020-12-23 10:41:42 +0900

    delete unnecessary variable

--- push-deeplink/DeeplinkApp.swift
+++ push-deeplink/DeeplinkApp.swift
@@ -3,12 +3,11 @@ import SwiftUI
 @main
 struct DeeplinkApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    private var screenCoordinator = ScreenCoordinator()
 
     var body: some Scene {
         WindowGroup {
             RootView()
-                .environmentObject(screenCoordinator)
+                .environmentObject(ScreenCoordinator())
         }
     }
 }
