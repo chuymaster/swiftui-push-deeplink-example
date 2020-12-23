133f29a59b0158b4123a32d4c49835e9d6058af4
chuymaster <chatchai.lokniyom@quipper.com>
2020-12-23 10:41:50 +0900

    auto indent

--- push-deeplink/AppDelegate.swift
+++ push-deeplink/AppDelegate.swift
@@ -5,9 +5,9 @@ import UIKit
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UNUserNotificationCenter.current()
-          .requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
-            print("Permission granted: \(granted)")
-          }
+            .requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
+                print("Permission granted: \(granted)")
+            }
         UNUserNotificationCenter.current().delegate = self
         return true
     }
@@ -15,16 +15,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
 
-  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-    completionHandler([.banner, .sound])
-  }
-
-  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-    guard let urlString = response.notification.request.content.userInfo["url"] as? String,
-          let url = URL(string: urlString) else {
-      return
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
     }
-    UIApplication.shared.open(url)
-    completionHandler()
-  }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        guard let urlString = response.notification.request.content.userInfo["url"] as? String,
+              let url = URL(string: urlString) else {
+            return
+        }
+        UIApplication.shared.open(url)
+        completionHandler()
+    }
 }
